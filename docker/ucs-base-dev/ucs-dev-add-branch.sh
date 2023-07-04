64f76600ed16498b273cdb9e980e4cd1d60cf08e
Thomas Kintscher <thomas.kintscher.extern@univention.de>
2023-07-04 22:14:14 +0200

    chore: make sources.list preference more obvious

--- docker/ucs-base-dev/ucs-dev-add-branch.sh
+++ docker/ucs-base-dev/ucs-dev-add-branch.sh
@@ -2,5 +2,5 @@
 
 for branch_slug in "$@"
 do
-    echo "deb http://omar.knut.univention.de/build2/git/${branch_slug} git main" >> /etc/apt/sources.list.d/15_ucs-dev-sources.list
+    echo "deb http://omar.knut.univention.de/build2/git/${branch_slug} git main" >> /etc/apt/sources.list.d/10_ucs-dev-sources.list
 done
